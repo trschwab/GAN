52305f7ec83885d54a4899bc1bebfeb2bfde452a
Dan <dtk008@bucknell.edu>
2018-11-12 20:13:57 -0500

    Unfinished color attempt

--- tutorial.py
+++ tutorial.py
@@ -5,11 +5,12 @@ from tqdm import tqdm
 import math
 
 
-from keras.layers import Input
+from keras.layers import Input, Reshape, Conv2D, Flatten, Activation
+from keras.layers import BatchNormalization, UpSampling2D
 from keras.models import Model, Sequential
 from keras.layers.core import Dense, Dropout
 from keras.layers.advanced_activations import LeakyReLU
-from keras.datasets import mnist, cifar10
+from keras.datasets import mnist, cifar10, fashion_mnist
 from keras.optimizers import Adam
 from keras import initializers
 
@@ -29,7 +30,15 @@ image_size = 1
 
 def load_minst_data():
     # load the data
-    (x_train, _), (_, _) = cifar10.load_data()
+    new_x = []
+    (x_train, labels), (_, _) = cifar10.load_data()
+    for i in range(len(labels)):
+        if labels[i][0] == 0:
+            new_x.append(x_train[i])
+
+    x_train = np.array(new_x)
+
+    print(len(x_train))
 
     #Setup the dimensions of the images
     global image_width
@@ -41,13 +50,12 @@ def load_minst_data():
     if len(x_train.shape) > 3:
         image_depth = x_train.shape[3]
     #Find the dimension of the image flattened out
-    image_size = image_height * image_width * image_depth
+    image_size = image_height * image_width
 
     # normalize our inputs to be in the range[-1, 1]
     x_train = (x_train.astype(np.float32) - 127.5)/127.5
     # convert x_train with a shape of (60000, 28, 28) to (60000, 784) so we have
     # 784 columns per row
-    x_train = x_train.reshape(x_train.shape[0], image_size)
     return x_train
 
 # You will use the Adam optimizer
@@ -56,35 +64,65 @@ def get_optimizer():
 
 def get_generator(optimizer):
     generator = Sequential()
-    generator.add(Dense(256, input_dim=random_dim, kernel_initializer=initializers.RandomNormal(stddev=0.02)))
-    generator.add(LeakyReLU(0.2))
+    dropout_prob = 0.4
 
-    generator.add(Dense(512))
-    generator.add(LeakyReLU(0.2))
+    generator.add(Dense(4*4*256, input_dim=100))
+    generator.add(BatchNormalization(momentum=0.9))
+    generator.add(Activation('relu'))
+    generator.add(Reshape((4,4,256)))
+    generator.add(Dropout(dropout_prob))
 
-    generator.add(Dense(1024))
-    generator.add(LeakyReLU(0.2))
+    generator.add(UpSampling2D())
+    generator.add(Conv2D(128, 5, padding='same'))
+    generator.add(BatchNormalization(momentum=0.9))
+    generator.add(Activation('relu'))
 
-    generator.add(Dense(image_size, activation='tanh'))
-    generator.compile(loss='binary_crossentropy', optimizer=optimizer)
+    generator.add(UpSampling2D())
+    generator.add(Conv2D(128, 5, padding='same'))
+    generator.add(BatchNormalization(momentum=0.9))
+    generator.add(Activation('relu'))
+
+    generator.add(UpSampling2D())
+    generator.add(Conv2D(64, 5, padding='same'))
+    generator.add(BatchNormalization(momentum=0.9))
+    generator.add(Activation('relu'))
+
+    generator.add(Conv2D(32, 5, padding='same'))
+    generator.add(BatchNormalization(momentum=0.9))
+    generator.add(Activation('relu'))
+
+    generator.add(Conv2D(3, 5, padding='same'))
+    generator.add(Activation('sigmoid'))
+
+    generator.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
     return generator
 
 def get_discriminator(optimizer):
     discriminator = Sequential()
-    discriminator.add(Dense(1024, input_dim=image_size, kernel_initializer=initializers.RandomNormal(stddev=0.02)))
-    discriminator.add(LeakyReLU(0.2))
-    discriminator.add(Dropout(0.3))
+    input_shape = (32, 32, 3)
+    dropout_prob = 0.4
 
-    discriminator.add(Dense(512))
-    discriminator.add(LeakyReLU(0.2))
-    discriminator.add(Dropout(0.3))
+    discriminator.add(Conv2D(32, 5, strides=2, input_shape=input_shape, padding='same'))
+    discriminator.add(LeakyReLU())
 
-    discriminator.add(Dense(256))
-    discriminator.add(LeakyReLU(0.2))
-    discriminator.add(Dropout(0.3))
+    discriminator.add(Conv2D(64, 5, strides=2, padding='same'))
+    discriminator.add(LeakyReLU())
+    discriminator.add(Dropout(dropout_prob))
 
-    discriminator.add(Dense(1, activation='sigmoid'))
-    discriminator.compile(loss='binary_crossentropy', optimizer=optimizer)
+    discriminator.add(Conv2D(128, 5, strides=2, padding='same'))
+    discriminator.add(LeakyReLU())
+    discriminator.add(Dropout(dropout_prob))
+
+    discriminator.add(Conv2D(256, 5, strides=1, padding='same'))
+    discriminator.add(LeakyReLU())
+    discriminator.add(Dropout(dropout_prob))
+
+    discriminator.add(Flatten())
+    discriminator.add(Dense(1))
+    discriminator.add(Activation('sigmoid'))
+
+    discriminator.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
     return discriminator
 
 def get_gan_network(discriminator, random_dim, generator, optimizer):
@@ -105,12 +143,12 @@ def get_gan_network(discriminator, random_dim, generator, optimizer):
 def plot_generated_images(epoch, generator, examples=100, dim=(10, 10), figsize=(10, 10)):
     noise = np.random.normal(0, 1, size=[examples, random_dim])
     generated_images = generator.predict(noise)
-    generated_images = generated_images.reshape(examples, image_width, image_height, image_depth)
+    print(generated_images.shape)
 
     plt.figure(figsize=figsize)
     for i in range(generated_images.shape[0]):
         plt.subplot(dim[0], dim[1], i+1)
-        plt.imshow(generated_images[i], interpolation='nearest', cmap='gray_r')
+        plt.imshow(generated_images[i], interpolation='nearest', cmap='gray')
         plt.axis('off')
     plt.tight_layout()
     plt.savefig('gan_generated_image_epoch_%d.png' % epoch)
@@ -131,7 +169,7 @@ def train(epochs=1, batch_size=128):
         print('-'*15, 'Epoch %d' % e, '-'*15)
         for _ in tqdm(range(math.ceil(batch_count))):
             # Get a random set of input noise and images
-            noise = np.random.normal(0, 1, size=[batch_size, random_dim])
+            noise = np.random.normal(-1.0, 1.0, size=[batch_size, random_dim])
             image_batch = x_train[np.random.randint(0, x_train.shape[0], size=batch_size)]
 
             # Generate fake MNIST images
@@ -153,9 +191,9 @@ def train(epochs=1, batch_size=128):
             discriminator.trainable = False
             gan.train_on_batch(noise, y_gen)
 
-        if e == 1 or e % 20 == 0:
+        if e == 1 or e % 5 == 0:
             plot_generated_images(e, generator)
     plot_generated_images(epochs, generator)
 
 if __name__ == '__main__':
-    train(5, 128)
+    train(40, 128)
